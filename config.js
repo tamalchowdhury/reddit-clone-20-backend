b9dbb1b6a2089935a6eae43545b83b97325e513d
Tamal Anwar <tamalanwaronline@gmail.com>
2019-03-08 20:53:12 +0600

    Added temp setting

--- config.js
+++ config.js
@@ -2,15 +2,15 @@ const config = {};
 
 config.development = {
   name: 'development',
-  httpPort: 5000,
-  httpsPort: 5001,
-  database: 'mongodb://localhost/reddit-clone-20',
+  httpPort: 8080,
+  httpsPort: 8081,
+  database: 'mongodb://tamal:reddit2@ds119930.mlab.com:19930/reddit-clone-20',
   secret: 'coolcake'
 };
 
 config.production = {
   name: 'production',
-  httpPort: 80,
+  httpPort: 8080,
   httpsPort: 443,
   database: 'mongodb://tamal:reddit2@ds119930.mlab.com:19930/reddit-clone-20',
   secret: 'whateverIlike'
